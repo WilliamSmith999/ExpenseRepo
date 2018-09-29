c81f3c2dc60004cf5e07b12206d16588658e2994
WilliamSmith999 <williamsmithtannis013@gmail.com>
2018-09-28 20:24:17 -0500

    fifth

--- CategoricalExpenses/CategoriesViewController.swift
+++ CategoricalExpenses/CategoriesViewController.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoriesViewController: UIViewController {
 
     @IBOutlet weak var categoriesTableView: UITableView!
+    
+    var catagories: [Catagories] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +21,20 @@ class CategoriesViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest: NSFetchRequest<Catagories> = Catagories.fetchRequest()
+        do{
+           catagories = try managedContext.fetch(fetchRequest)
+            categoriesTableView.reloadData()
+        }catch{
+            print("Could not load")
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -27,11 +44,14 @@ class CategoriesViewController: UIViewController {
 
 extension CategoriesViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return catagories.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = categoriesTableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath)
+        let category = catagories[indexPath.row]
+        
+        cell.textLabel?.text = category.title
         
         return cell
     }
