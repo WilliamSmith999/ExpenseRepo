be0e642f9dc7a338b7c3dc1b100e6368f064b990
WilliamSmith999 <williamsmithtannis013@gmail.com>
2018-09-28 19:31:56 -0500

    first

---
+++ CategoricalExpenses/Catagories+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Catagories+CoreDataClass.swift
+//  CategoricalExpenses
+//
+//  Created by William Smith on 9/28/18.
+//  Copyright © 2018 Shawn Moore. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Catagories)
+public class Catagories: NSManagedObject {
+
+}
---
+++ CategoricalExpenses/Expense+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Expense+CoreDataClass.swift
+//  CategoricalExpenses
+//
+//  Created by William Smith on 9/28/18.
+//  Copyright © 2018 Shawn Moore. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Expense)
+public class Expense: NSManagedObject {
+
+}
